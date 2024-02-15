5519fd5a8324092cb851ce3d3690e8e4776237fe
= <=>
2024-02-15 12:41:44 +0100

    Repetir

--- piedrapapelotijera.py
+++ piedrapapelotijera.py
@@ -2,36 +2,39 @@ import random
 
 print("Piedra, papel o tijera, JAMG")
 
-print("1 = piedra")
-print("2 = papel")
-print("3 = tijera")
-    
-tirada = int(input("escoge tu tirada: "))
+def bucle():
+    print("1 = piedra")
+    print("2 = papel")
+    print("3 = tijera")
+        
+    tirada = int(input("escoge tu tirada: "))
 
-tiradaordenador = random.randint(1,3)
+    tiradaordenador = random.randint(1,3)
 
-if tiradaordenador == 1:
-    print("El ordenador ha sacado piedra")
-elif tiradaordenador == 2:
-    print("El ordenador ha sacado papel")
-elif tiradaordenador == 3:
-    print("El ordenador ha sacado tijera")
+    if tiradaordenador == 1:
+        print("El ordenador ha sacado piedra")
+    elif tiradaordenador == 2:
+        print("El ordenador ha sacado papel")
+    elif tiradaordenador == 3:
+        print("El ordenador ha sacado tijera")
 
-if tirada == 1 and tiradaordenador == 1:
-    print("empate")
-elif tirada == 1 and tiradaordenador == 2:
-    print("maquina gana")
-elif tirada == 1 and tiradaordenador == 3:
-    print("jugador gana")
-elif tirada == 2 and tiradaordenador == 1:
-    print("jugador gana")
-elif tirada == 2 and tiradaordenador == 2:
-    print("empate")
-elif tirada == 2 and tiradaordenador == 3:
-    print("maquina gana")
-elif tirada == 3 and tiradaordenador == 1:
-    print("maquina gana")
-elif tirada == 3 and tiradaordenador == 2:
-    print("jugador gana")
-elif tirada == 3 and tiradaordenador == 3:
-    print("empate")
+    if tirada == 1 and tiradaordenador == 1:
+        print("empate")
+    elif tirada == 1 and tiradaordenador == 2:
+        print("maquina gana")
+    elif tirada == 1 and tiradaordenador == 3:
+        print("jugador gana")
+    elif tirada == 2 and tiradaordenador == 1:
+        print("jugador gana")
+    elif tirada == 2 and tiradaordenador == 2:
+        print("empate")
+    elif tirada == 2 and tiradaordenador == 3:
+        print("maquina gana")
+    elif tirada == 3 and tiradaordenador == 1:
+        print("maquina gana")
+    elif tirada == 3 and tiradaordenador == 2:
+        print("jugador gana")
+    elif tirada == 3 and tiradaordenador == 3:
+        print("empate")
+    bucle()
+bucle()
